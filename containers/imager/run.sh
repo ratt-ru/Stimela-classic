a47f566dbb97cab4efef9c73c06db011000918f1
sphemakh <sphemakh@gmail.com>
2015-09-29 18:29:27 +0000

    Fix Dockerfile. Accept config as ENV variable

--- containers/imager/run.sh
+++ containers/imager/run.sh
@@ -1,22 +1,12 @@
 #!/bin/bash -ve
 
-if [ -z "$1" ]; then 
-    echo "MS not specified. Aborting"
-    exit
-else
-    MSNAME="$1"
-fi
-
-
-if [ -z "$2" ]; then 
-    CONFIG="$2"
-else
-    CONFIG="imaging_params.json"
-fi
-
 
 if [ -z "$USER" ]; then
     export USER=root
+fi
+
+if [ -z "$CONFIG" ]; then 
+    export CONFIG="imaging_params.json"
 fi
 
 pyxis $MSNAME CFG=$CONFIG OUTDIR=/output azishe

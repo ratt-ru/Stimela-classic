3a3850b9a707505cb7c98267698b3b5819060d0c
sphemakh <sphemakh@gmail.com>
2016-04-15 16:26:32 +0000

    update options parsed to tdlconf

--- stimela/cargo/cab/calibrator/src/pyxis-run.py
+++ stimela/cargo/cab/calibrator/src/pyxis-run.py
@@ -37,6 +37,8 @@ def calibrate(jdict):
 
     x.sh("addbitflagcol $MS")
 
+    ms.set_default_spectral_info()
+
     prefix = jdict.get("prefix", None)
 
     for item in [INDIR, "/data/skymodels/"]:
@@ -53,32 +55,76 @@ def calibrate(jdict):
 
     options = {}
     options["tiggerlsm.lsm_subset"] = jdict.get("subset", "all")
-    gtimeint, gfreqint = jdict.get("gjones_intervals", (1,1))
+    gtimeint, gfreqint = jdict.get("Gjones_intervals", (1,1))
 
     options["stefcal_gain.timeint"] = gtimeint
     options["stefcal_gain.freqint"] = gfreqint
-    options["stefcal_gain.flag_chisq_threshold"] = jdict.get("gjones_thresh_sigma", 10)
-    options["stefcal_gain.flag_ampl_low"] = jdict.get("gjones_flag_ampl_low", 0.5)
-    options["stefcal_gain.flag_ampl_high"] = jdict.get("gjones_flag_ampl_high", 2)
+    options["stefcal_gain.flag_ampl"] = jdict.get("gjones_ampl_clipping", 0)
+    options["stefcal_gain.flag_chisq_threshold"] = jdict.get("Gjones_thresh_sigma", 10)
+    options["stefcal_gain.flag_ampl_low"] = jdict.get("Gjones_flag_ampl_low", 0.3)
+    options["stefcal_gain.flag_ampl_high"] = jdict.get("Gjones_flag_ampl_high", 2)
 
-    stefcal.STEFCAL_DIFFGAIN_INTERVALS = jdict.get("ejones_intervals", None)
-    stefcal.STEFCAL_DIFFGAIN_SMOOTHING = jdict.get("ejones_smoothing", None)
-    stefcal.STEFCAL_GAIN_SMOOTHING = jdict.get("gjones_smoothing", None)
+    stefcal.STEFCAL_DIFFGAIN_INTERVALS = jdict.get("DDjones_intervals", None)
+    stefcal.STEFCAL_DIFFGAIN_SMOOTHING = jdict.get("DDjones_smoothing", None)
+    stefcal.STEFCAL_GAIN_SMOOTHING = jdict.get("Gjones_smoothing", None)
 
-    ejones = jdict.get("ejones", False)
+    beam = jdict.get("Ejones", False)
 
-    beam = jdict.get("add_beam", False)
     if beam:
         options["me.e_enable"] = 1
         options["me.p_enable"] = 1
         options["me.e_module"] = "Siamese_OMS_pybeams_fits"
+        options["pybeams_fits.l_axis"] = jdict.get("beam_l_axis", "L")
+        options["pybeams_fits.m_axis"] = jdict.get("beam_m_axis", "M")
         options["pybeams_fits.filename_pattern"] =  "%s/%s"%(INDIR, jdict["beam_files_pattern"])
 
     options["ms_sel.input_column"] = column
 
+    DDjones = jdict.get("DDjones", False)
+
+    if jdict.get("IFR_gains"):
+        options["stefcal_ifr_gains"] = 1
+    
+    gains = {
+        "Gjones" : "gain",
+        "DDjones" : "diffgain",
+        "IFRjones" : "ifrgain"
+    }
+
+    reset = jdict.get("reset", None)
+    if reset :
+        if isinstance(reset, (str, unicode)):
+            reset = map(str, reset.split(","))
+
+        for item in reset:
+            if item =="all":
+                options["stefcal_reset_all"] = True
+                break
+            else:
+                options["%s_reset"%gains[item]] = True
+
+
+    apply_ = jdict.get("apply", None)
+    if apply_ :
+        if isinstance(apply_, (str, unicode)):
+            apply_ = map(str, apply_.split(","))
+    
+        for item in apply_:
+            if item=="all":
+                options["gain_apply_only"] = True
+                options["ifrgain_apply_only"] = True
+                options["diffgain_apply_only"] = True
+                break
+            else:
+                options["%s_apply_only"%gains[item]] = True
+
+    # Include UV model if specified
+    if jdict.pop("add_uvmodel", False):
+        options.update( {'read_ms_model':1,'ms_sel.model_column':'MODEL_DATA'} )
+
     stefcal.stefcal(section="stefcal", gain_plot_prefix=prefix,
                     reset=True, dirty=False, 
-                    diffgains=ejones,
+                    diffgains=DDjones,
                     options=options,
                     output=jdict.get("output_column", "CORR_RES"))
 

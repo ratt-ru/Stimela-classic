97af58aea970d33448154267362014d7d729cc7c
sphemakh <sphemakh@gmail.com>
2016-04-15 16:25:22 +0000

    allow multiple MSs for flaging

--- stimela/cargo/cab/autoflagger/src/run.py
+++ stimela/cargo/cab/autoflagger/src/run.py
@@ -7,9 +7,15 @@ import utils
 
 CONFIG = os.environ["CONFIG"]
 INPUT = os.environ["INPUT"]
+MSDIR = os.environ["MSDIR"]
 
 jdict = utils.readJson(CONFIG)
-msname = "%s/%s"%("/msdir", jdict["msname"])
+msname = jdict["msname"]
+
+if isinstance(msname, (str, unicode)):
+    msname = [msname]
+
+msname = " ".join( ["%s/%s"%(MSDIR , ms)  for ms in msname] )
 
 strategy = jdict.get("strategy", None)
 strategy = "-s %s/%s"%(INPUT, strategy) if strategy else ""
--- stimela/cargo/cab/flagms/src/run.py
+++ stimela/cargo/cab/flagms/src/run.py
@@ -8,9 +8,20 @@ import utils
 CONFIG = os.environ["CONFIG"]
 
 jdict = utils.readJson(CONFIG)
-msname = "%s/%s"%(os.environ["MSDIR"], utils.pop["msname"])
+
+msnames = jdict["msname"]
+
+if isinstance(msnames, (str, unicode)):
+    msnames = [msnames]
+
+msname = [ "%s/%s"%(os.environ["MSDIR"], ms) for ms in msnames ]
 
 
-flag_cmd = jdict.pop("command")
+flag_cmd = jdict.pop("command", "")
 
-utils.xrun("flag-ms.py", [flag_cmd, msname])
+def flagms(ms):
+    utils.xrun("flag-ms.py", [flag_cmd, ms])
+
+ncpu = jdict.get("ncpu", 1)
+
+utils.pper(msname, flagms, ncpu)

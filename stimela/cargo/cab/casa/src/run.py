1481615ce7ce9017b2a92216d373cb0bbb5d294d
sphemakh <sphemakh@gmail.com>
2016-10-19 12:33:36 +0000

    fix exportfits convertion when using nternms>1

--- stimela/cargo/cab/casa/src/run.py
+++ stimela/cargo/cab/casa/src/run.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import re
+import glob
 
 sys.path.append("/utils")
 import utils
@@ -50,21 +51,34 @@ vis = options.pop("msname")
 prefix = options["imagename"] = OUTPUT + "/" + options.pop("imagename", os.path.basename(vis[:-3]))
 options["vis"] = MSDIR + "/" + vis
 
-images = ["flux", "model", "residual", "psf", "image", "tt0", "alpha", "alpha.error", "beta", "beta.error"]
+nterms = jdict.get("nterms", 0)
+images = ["flux", "model", "residual", "psf", "image"]
 STD_IMAGES = images[:4]
 
 keep_casa_images = options["keep_casa_images"]
 utils.icasa("clean", **options)
 
-for image in images[:5]:
+for image in images:
     img ="{:s}.{:s}".format(prefix, image)
-    fits = img + ".fits" if image!="image" else img.replace(".image", ".restored.fits")
+    if nterms:
+        _images = ["%s.tt%d"%(img,d) for d in range(nterms)]
+        if image=="image":
+            if  nterms==2:
+                alpha = img+".alpha"
+                alpha_err = img+".alpha.error"
+                _images += [alpha,alpha_err]
+            if  nterms==3:
+                beta = img+".beta"
+                beta_err = img+".beta.error"
+                _images += [beta,beta_err]
+    else:
+        _images = [img]
 
-    if image is STD_IMAGES and (not os.path.exists(img)):
-        raise RuntimeError("Standard output from CASA clean task not found. Something went wrong durring cleaning, take a look at the logs and such")
-
-    elif os.path.exists(img):
-        utils.icasa("exportfits", imagename=img, fitsimage=fits)
+    for _image in  _images:
+        if _image is STD_IMAGES and (not os.path.exists(_image)):
+            raise RuntimeError("Standard output from CASA clean task not found. Something went wrong durring cleaning, take a look at the logs and such")
+        elif os.path.exists(_image):
+            utils.icasa("exportfits", imagename=_image, fitsimage=_image+".fits")
 
         if not keep_casa_images:
             utils.xrun("rm", ["-rf", img])

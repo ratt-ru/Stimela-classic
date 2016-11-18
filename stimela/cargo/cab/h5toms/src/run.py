4e0c68ff4b5ce5f2c60bb056f57cb498e6fa431f
Simon Perkins <simon.perkins@gmail.com>
2016-11-18 13:46:49 +0200

    h5toms.py supporting files

---
+++ stimela/cargo/cab/h5toms/src/run.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+sys.path.append("/utils")
+import utils
+
+
+CONFIG = os.environ["CONFIG"]
+INDIR = os.environ["INPUT"]
+OUTDIR = os.environ["OUTPUT"]
+MAC_OS = os.environ["MAC_OS"]
+
+if MAC_OS.lower() in ["yes", "true", "yebo", "1"]:
+    MAC_OS = True
+else:
+    MAC_OS = False
+
+jdict = utils.readJson(CONFIG)
+
+# Create options for boolean flags
+flags = ["full_pol", "flag_av"]
+
+flags = ['--%s' % (f,) for f, v in (
+    (f, jdict.pop(f, False)) for f in flags)
+    if v is True]
+
+cmd = [ "--%s %s"%(key, "" if isinstance(val, bool) else val) for (key,val) in jdict.iteritems()]
+
+utils.xrun("h5toms.py", cmd+flags )

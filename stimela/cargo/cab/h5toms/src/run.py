17e45431a5f8586cbb4eedc510a3c211a2e98e14
Simon Perkins <simon.perkins@gmail.com>
2016-12-01 18:33:31 +0200

    Support h5toms's output-ms argument
    Now that https://github.com/ska-sa/katdal/pull/64 is merged.

--- stimela/cargo/cab/h5toms/src/run.py
+++ stimela/cargo/cab/h5toms/src/run.py
@@ -25,8 +25,14 @@ flags = ['--%s' % (f,) for f, v in (
     (f, jdict.pop(f, False)) for f in flags)
     if v is True]
 
-h5s = ["%s/%s" % (INPUT, n) for n in jdict.pop('hdf5files')]
+h5s = [os.path.join(INPUT, n) for n in jdict.pop('hdf5files')]
+
+try:
+    ms = ["--output-ms %s" % os.path.join(MSDIR, jdict.pop('output-ms'))]
+except KeyError:
+    files = [os.splitext(os.path.split(f)[1])[0] for f in h5s]
+    ms = ["--output-ms %s.ms" % os.path.join(MSDIR, f) for f in files[0:1]]
 
 cmd = [ "--%s %s"%(key, "" if isinstance(val, bool) else val) for (key,val) in jdict.iteritems()]
 
-utils.xrun("h5toms.py", cmd+flags+h5s )
+utils.xrun("h5toms.py", cmd+flags+ms+h5s )

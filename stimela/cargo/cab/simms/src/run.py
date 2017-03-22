37ea1d951cf9507cd92b8af94c993a4b13afedd1
SpheMakh <sphemakh@gmail.com>
2017-03-22 12:48:46 +0200

    fix parsing of custom antenna table

--- stimela/cargo/cab/simms/src/run.py
+++ stimela/cargo/cab/simms/src/run.py
@@ -17,31 +17,11 @@ _positional = ['antenna-file']
 positional = []
 for item in _positional:
     param = filter( lambda a: a['name']==item, params)[0]
-    if param['value']:
-        positional.append(param['value'])
+    value = param['value']
+    params.remove(param)
 
-#f params.pop("from-fits", False) and imagename:
-#   with pyfits.open(imagename) as hdu:
-#       hdr = hdu[0].header
-#       naxis = hdr["naxis"]
-
-#       freq = filter(lambda ind: hdr["ctype%d"%ind].startswith("FREQ"),
-#               range(2, naxis+1, 1))
-#       if freq:
-#           freq = freq[0]
-#       else:
-#           raise TypeError("Your FITS image has no frequency information")
-
-#       nchan = hdr["naxis%d"%freq]
-#       freq0 = hdr["crval%d"%freq]
-#       dfreq = abs(hdr["cdelt%d"%freq])
-
-#       params["freq0"] = freq0
-#       params["dfreq"] = dfreq
-#       params["nchan"] = nchan
-
-#       if not direction:
-#           params["direction"] = "J2000,%fdeg,%fdeg"%(hdr["crval1"], hdr["crval2"])
+    if value:
+        positional.append(value)
 
 args = []
 for param in params:

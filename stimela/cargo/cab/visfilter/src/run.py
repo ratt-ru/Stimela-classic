59b9c37fbde46c025c9858215ac49d28a1b091ae
SpheMakh <sphemakh@gmail.com>
2017-03-23 14:10:13 +0200

    add visfilter cab

--- stimela/cargo/cab/visfilter/src/run.py
+++ stimela/cargo/cab/visfilter/src/run.py
@@ -10,17 +10,34 @@ INPUT = os.environ["INPUT"]
 OUTPUT = os.environ["OUTPUT"]
 MSDIR = os.environ["MSDIR"]
 
-jdict = utils.readJson(CONFIG)
+cab = utils.readJson(CONFIG)
+params = {}
 
-msname = "%s/%s"%(MSDIR, jdict["msname"])
-res = jdict["res"]
-freq = jdict["freq"]
-reset = jdict.get("reset", False)
+msname = None
+res = None
+freq = None
+reset = False
+savefig = None
 
+for param in cab['parameters']:
+    name = param['name']
+    value = param['value']
 
-savefig = "%s/%s_%.2f-%.2f.png"%(OUTPUT, jdict["msname"][:-3], res[0], res[1])
-
-taper.taper(msname, res=res, freq=freq, savefig=savefig)
+    if name == 'min-max-resolution':
+        res = value
+    elif name == 'frequency':
+        freq = value
+    elif name == 'reset':
+        reset = value
+    elif name == 'msname':
+        msname = value
+    elif name == 'save-figure':
+        savefig = value
+        
 
 if reset:
     taper.reset(msname)
+    sys.exit(0)
+
+taper.taper(msname, res=res, freq=freq, savefig=savefig)
+

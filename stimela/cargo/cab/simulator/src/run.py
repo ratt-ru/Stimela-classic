29cffac66c9ee10aff11bfb6d660f591bc0e41e1
kat <kat@ska.ac.za>
2017-06-23 15:30:32 +0200

    add comma

--- stimela/cargo/cab/simulator/src/run.py
+++ stimela/cargo/cab/simulator/src/run.py
@@ -90,7 +90,7 @@ if beam and beam_files_pattern:
         "me.e_enable"   : 1,
         "me.p_enable"   : 1,
         "me.e_module"   : "Siamese_OMS_pybeams_fits",
-        "pybeams_fits.sky_rotation"    :  params.pop('parallactic-angle-rotation', False)
+        "pybeams_fits.sky_rotation"    :  params.pop('parallactic-angle-rotation', False),
         "me.e_all_stations" : 1,
         "pybeams_fits.l_axis"   : params.pop("beam-l-axis", "L"),
         "pybeams_fits.m_axis"   : params.pop("beam-m-axis", "M"),

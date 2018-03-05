d6d743cfb1b73dccc52d3f86726c8fc5ea44f5a9
athanaseus <aramaila@ska.ac.za>
2018-03-05 10:08:32 +0200

    Ensure backward compatibility of scripts

--- stimela/cargo/cab/wsclean/src/run.py
+++ stimela/cargo/cab/wsclean/src/run.py
@@ -42,9 +42,13 @@ elif not (isinstance(trim, list) and
           len(npix) == 2) :
     raise ValueError("trim only accepts single int or list[2] of int")
 
-trindex = filter(lambda x: x['name'] == 'trim', params)[0]
-params.remove(trindex)
-filter(lambda x: x['name'] == 'size', params)[0]["value"] = trim #npix is now the unpadded size
+padding = filter(lambda x: x['name'] == 'padding', params)
+if not padding:
+    trindex = filter(lambda x: x['name'] == 'trim', params)[0]
+    params.remove(trindex)
+    pad = max(npix[0], npix[1]) / float(min(trim[0], trim[1]))
+    filter(lambda x: x['name'] == 'size', params)[0]["value"] = trim  # npix is now the unpadded size
+    params.append({'name': 'padding', 'value': pad})  # replace with 'padding' argument
 
 for param in params:
     name = param['name']

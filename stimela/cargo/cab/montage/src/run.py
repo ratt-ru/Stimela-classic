8363dca918b4ac6dc673b9d6d25708e84c6b5bad
Fil8 <filippo.maccagni@gmail.com>
2018-10-08 16:20:31 +0200

    montage mosaicing update

--- stimela/cargo/cab/montage/src/run.py
+++ stimela/cargo/cab/montage/src/run.py
@@ -1,9 +1,9 @@
 import os
 import sys
-
+import subprocess
 sys.path.append('/utils')
 import utils
-import montage_wrapper as montage
+#import montage_wrapper as montage
 
 
 CONFIG = os.environ["CONFIG"]
@@ -19,9 +19,33 @@ for param in cab['parameters']:
 
     if value is None:
         continue
-    if name == "output_dir":
-        args["output_dir"] = os.path.join(OUTPUT, value)
-
+    #if name == "output_dir":
+    #    args["output_dir"] = os.path.join(OUTPUT, value)
+    #if name == 'input_dir':
+    #    args['input_dir'] = os.path.join(INPUT,value)
     args[name] = value
 
-montage.mosaic(**args)
+if os.path.exists(OUTPUT+'/mask_mosaic')==False:
+    os.mkdir(OUTPUT+'/mask_mosaic')
+
+outdir = OUTPUT+'/mask_mosaic'
+
+
+make_table = ['mImgtbl',args['input_dir'],outdir+'/mosaic_table.tbl'] 
+subprocess.check_output(make_table)
+
+make_header = ['mMakeHdr',outdir+'/mosaic_table.tbl',outdir+'/mosaic_header.hdr']
+subprocess.check_output(make_header)
+
+project_mosaic = ['mProjExec', '-p',args['input_dir'],outdir+'/mosaic_table.tbl',outdir+'/mosaic_header.hdr',args['output_dir'],outdir+'/stats.tbl']
+subprocess.check_output(project_mosaic)
+
+make_mosaic_table = ['mImgtbl',args['output_dir'],outdir+'/mosaic_table2.tbl']
+subprocess.check_output(make_mosaic_table)
+
+make_mosaic=['mAdd', '-p', args['input_dir'], outdir+'/mosaic_table2.tbl',outdir+'/mosaic_header.hdr',OUTPUT+'/mosaic.fits']
+subprocess.check_output(make_mosaic)
+
+#command = 'mProjExec '+OUTPUT+'/mosaic_table.tbl '+OUTPUT+'/mosaic_header.hdr '+args['output_dir']+' '+OUTPUT+'/mosaic_stats.tbl'
+#print command
+#os.system(command)

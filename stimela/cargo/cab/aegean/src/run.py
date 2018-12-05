53de905e39674a6db7f6750077beee993c7cb9ec
athanaseus <aramaila@ska.ac.za>
2018-12-05 15:16:04 +0200

    Add parameter to allow porting output model to tigger format

--- stimela/cargo/cab/aegean/src/run.py
+++ stimela/cargo/cab/aegean/src/run.py
@@ -1,5 +1,12 @@
 import os
 import sys
+import numpy
+import Tigger
+import tempfile
+import pyfits
+
+from astLib.astWCS import WCS
+from Tigger.Models import SkyModel, ModelClasses
 
 sys.path.append('/scratch/stimela')
 import utils
@@ -9,6 +16,12 @@ INPUT = os.environ["INPUT"]
 MSDIR = os.environ["MSDIR"]
 
 cab = utils.readJson(CONFIG)
+
+write_catalog = ['port2tigger', 'table']
+
+write_opts = {}
+img_opts = {}
+
 args = []
 
 for param in cab['parameters']:
@@ -18,10 +31,83 @@ for param in cab['parameters']:
         continue
     elif value is False:
         continue
-
     if name == 'filename':  # positional argument
         args += ['{0}'.format(value)]
     else:
-        args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
+        if name != "port2tigger":
+            args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
+    if name in write_catalog:
+        write_opts[name] = value
+    else:
+        img_opts[name] = value
 
 utils.xrun(cab['binary'], args)
+
+port2tigger = write_opts.pop('port2tigger')
+outfile = write_opts.pop('table')
+image = img_opts.pop('filename')
+
+if port2tigger:
+    write_opts['format'] = 'fits'
+
+if not port2tigger:
+    sys.exit(0)
+
+
+# convert to data file to Tigger LSM
+# First make dummy tigger model
+tfile = tempfile.NamedTemporaryFile(suffix='.txt')
+tfile.flush()
+
+prefix = os.path.splitext(outfile)[0]
+tname_lsm = prefix + ".lsm.html"
+with open(tfile.name, "w") as stdw:
+    stdw.write("#format:name ra_d dec_d i emaj_s emin_s pa_d\n")
+
+model = Tigger.load(tfile.name)
+tfile.close()
+
+
+def tigger_src(src, idx):
+
+    name = "SRC%d" % idx
+    # TODO: (05-12-2018) USE from astropy.table import Table
+    flux = ModelClasses.Polarization(src["Total_flux"], 0, 0, 0, I_err=src["E_Total_flux"])
+    ra, ra_err = map(numpy.deg2rad, (src["RA"], src["E_RA"]))
+    dec, dec_err = map(numpy.deg2rad, (src["DEC"], src["E_DEC"]))
+    pos = ModelClasses.Position(ra, dec, ra_err=ra_err, dec_err=dec_err)
+    ex, ex_err = map(numpy.deg2rad, (src["DC_Maj"], src["E_DC_Maj"]))
+    ey, ey_err = map(numpy.deg2rad, (src["DC_Min"], src["E_DC_Min"]))
+    pa, pa_err = map(numpy.deg2rad, (src["PA"], src["E_PA"]))
+
+    if ex and ey:
+        shape = ModelClasses.Gaussian(ex, ey, pa, ex_err=ex_err, ey_err=ey_err, pa_err=pa_err)
+    else:
+        shape = None
+    source = SkyModel.Source(name, pos, flux, shape=shape)
+    # Adding source peak flux (error) as extra flux attributes for sources,
+    # and to avoid null values for point sources I_peak = src["Total_flux"]
+    if shape:
+        source.setAttribute("I_peak", src["Peak_flux"])
+        source.setAttribute("I_peak_err", src["E_peak_flux"])
+    else:
+        source.setAttribute("I_peak", src["Total_flux"])
+        source.setAttribute("I_peak_err", src["E_Total_flux"])
+
+    return source
+
+
+with pyfits.open(outfile.split('.')[0]+"_comp."+outfile.split('.')[-1]) as hdu:
+    data = hdu[1].data
+
+    for i, src in enumerate(data):
+        print src
+        model.sources.append(tigger_src(src, i))
+
+wcs = WCS(image)
+centre = wcs.getCentreWCSCoords()
+model.ra0, model.dec0 = map(numpy.deg2rad, centre)
+
+model.save(tname_lsm)
+# Rename using CORPAT
+utils.xrun("tigger-convert", [tname_lsm, "--rename -f"])

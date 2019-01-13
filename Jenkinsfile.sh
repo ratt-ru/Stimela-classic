8e09862fca71abe48299de5ae799f466eb6e2787
bennahugo <bennahugo@aol.com>
2019-01-13 10:31:48 +0200

    Move jenkins configuration into repo

---
+++ Jenkinsfile.sh
@@ -0,0 +1,30 @@
+WORKSPACE_ROOT="$WORKSPACE/$BUILD_NUMBER"
+TEST_OUTPUT_DIR="$WORKSPACE_ROOT/test-output"
+TEST_DATA_DIR="$WORKSPACE/../../../test-data"
+mkdir $TEST_OUTPUT_DIR
+
+#Custom home for this run's temporary stuff
+HOME=$WORKSPACE_ROOT
+export HOME
+
+# Install Stimela into a virtual env
+virtualenv ${WORKSPACE_ROOT}/projects/pyenv
+. ${WORKSPACE_ROOT}/projects/pyenv/bin/activate
+pip install pip setuptools -U
+PATH=${WORKSPACE}/projects/pyenv/bin:$PATH
+LD_LIBRARY_PATH=${WORKSPACE}/projects/pyenv/lib:$LD_LIBRARY_PATH
+pip install ${WORKSPACE_ROOT}/projects/Stimela/
+
+# Copy a clean dataset over
+mkdir $TEST_OUTPUT_DIR/msdir
+tar -xvf $TEST_DATA_DIR/12A-405.sb7601493.eb10633016.56086.127048738424.tgz -C $TEST_OUTPUT_DIR/msdir
+mkdir $TEST_OUTPUT_DIR/input
+cp -r $TEST_DATA_DIR/beams $TEST_OUTPUT_DIR/input/beams
+
+which stimela
+
+#Run forest run!
+cd $TEST_OUTPUT_DIR
+nosetests "${WORKSPACE_ROOT}/projects/Stimela/stimela/tests"
+
+

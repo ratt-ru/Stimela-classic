4f6a3e70ddc33f63c9c5bdb4e5515606f0f5233e
athanaseus <aramaila@ska.ac.za>
2019-02-21 13:32:38 +0200

    src file

--- stimela/cargo/cab/rfinder/src/run.py
+++ stimela/cargo/cab/rfinder/src/run.py
@@ -21,9 +21,8 @@ with open(rfinder_file) as f:
     list_doc = yaml.load(f)
 
 
-list_doc['general']['outdir'] = OUTPUT
-list_doc['general']['workdir'] = MSDIR
-
+list_doc['general']['outdir'] = '{:s}/'.format(OUTPUT)
+list_doc['general']['workdir'] = '{:s}/'.format(MSDIR)
 
 for param in cab['parameters']:
     name = param['name']
@@ -32,16 +31,29 @@ for param in cab['parameters']:
     if value is None:
         continue
 
-    for par in list_doc.keys():
-        if type(par) == dict:
-            for p in par.keys():
-                if p == name:
-                    list_doc[par][p] = value
-        else:
-            if par == name:
-                list_doc[par] = value
+    msnames = []
+    if name == 'msname':
+        for ms in value:
+            msnames.append(ms.split('/')[-1])
+        list_doc['general']['msname'] = msnames
+        continue
 
-with open(rfinder_file, "w") as f:
+    for key, val in list_doc.items():
+        if type(val) == dict:
+            for k1, v1 in val.items():
+                if type(v1) == dict:
+                    for k2, v2 in v1.items():
+                        if k2 == name:
+                            list_doc[key][k1][k2] = value
+                else:
+                    if k1 == name:
+                        list_doc[key][k1] = value
+        else:
+            if key == name:
+                list_doc[key] = value
+
+edited_file = 'rfinder_default.yml'
+with open(edited_file, "w") as f:
     yaml.dump(list_doc, f)
 
-utils.xrun('rfinder', [rfinder_file])
+utils.xrun('rfinder', [edited_file])

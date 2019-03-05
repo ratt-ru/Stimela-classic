7b65ded02265b584e59ab359b11435af730c5cf9
Benna Hugo <bennahugo@aol.com>
2019-03-05 13:54:35 +0200

    Add catdagger. Fixes #349

---
+++ stimela/cargo/cab/catdagger/src/run.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+sys.path.append('/scratch/stimela')
+import utils
+
+CONFIG = os.environ["CONFIG"]
+INPUT = os.environ["INPUT"]
+MSDIR = os.environ["MSDIR"]
+
+cab = utils.readJson(CONFIG)
+args = []
+
+
+for param in cab['parameters']:
+    name = param['name']
+    value = param['value']
+    if name == "noise-map": 
+        args += [value]
+        continue
+    if value is None:
+        continue
+    elif value is False:
+        continue
+    
+    args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
+
+utils.xrun(cab['binary'], args)

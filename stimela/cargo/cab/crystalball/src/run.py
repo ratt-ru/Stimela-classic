9b60ff71a7cc8164eb4a6733d3828b400e5d98dd
sphemakh <sphemakh@gmail.com>
2019-03-22 14:15:45 +0200

    add crystall ball

--- stimela/cargo/cab/crystalball/src/run.py
+++ stimela/cargo/cab/crystalball/src/run.py
@@ -1,7 +1,5 @@
 import os
 import sys
-import yaml
-import rfinder
 
 sys.path.append('/scratch/stimela')
 import utils
@@ -10,48 +8,22 @@ import utils
 CONFIG = os.environ["CONFIG"]
 INPUT = os.environ["INPUT"]
 MSDIR = os.environ["MSDIR"]
-OUTPUT = os.environ["OUTPUT"]
 
 cab = utils.readJson(CONFIG)
 args = []
 msname = None
-
-pkg_path = os.path.dirname(os.path.realpath(rfinder.__file__))
-rfinder_file = '{:s}/rfinder_default.yml'.format(pkg_path)
-
-with open(rfinder_file) as f:
-    list_doc = yaml.load(f)
-
-list_doc['general']['outdir'] = '{:s}/'.format(OUTPUT)
-list_doc['general']['workdir'] = '{:s}/'.format(MSDIR)
-
 for param in cab['parameters']:
     name = param['name']
     value = param['value']
 
     if value is None:
         continue
-
-    if name == 'msname':
-        list_doc['general']['msname'] = value.split('/')[-1]
+    elif value is False:
         continue
+    elif value is True:
+        value = ''
 
-    for key, val in list_doc.items():
-        if type(val) == dict:
-            for k1, v1 in val.items():
-                if type(v1) == dict:
-                    for k2, v2 in v1.items():
-                        if k2 == name:
-                            list_doc[key][k1][k2] = value
-                else:
-                    if k1 == name:
-                        list_doc[key][k1] = value
-        else:
-            if key == name:
-                list_doc[key] = value
+    args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
 
-edited_file = 'rfinder_default.yml'
-with open(edited_file, "w") as f:
-    yaml.dump(list_doc, f)
 
-utils.xrun('rfinder -c', [edited_file])
+utils.xrun(cab['binary'], args)

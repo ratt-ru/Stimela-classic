247b2d3f9ef72ac40bfd6e3d3c69c1f539712040
bennahugo <bennahugo@aol.com>
2019-04-30 09:05:52 +0200

    Various missing parameters and new ddf build

--- stimela/cargo/cab/cubical/src/run.py
+++ stimela/cargo/cab/cubical/src/run.py
@@ -25,13 +25,14 @@ for param in cab['parameters']:
     elif name == 'parset':
        parset = [value]
        continue
-    elif name == 'model-list':
-        if isinstance(value, str):
-            value = [value]
-        for i,val in enumerate(value):
-            if not os.path.exists(val.split("@")[0]):
-                value[i] = os.path.basename(val)
-        value = ':'.join(value)
+#   this is no longer valid - model list can be a more complex pattern
+#    elif name == 'model-list':
+#        if isinstance(value, str):
+#            value = [value]
+#        for i,val in enumerate(value):
+#            if not os.path.exists(val.split("@")[0]):
+#                value[i] = os.path.basename(val)
+#        value = ':'.join(value)
     elif isinstance(value, list):
         value = ",".join( map(str, value) )
 

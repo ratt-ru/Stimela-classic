ae591f6f0c5e28d1e8e05fbae6b4c11ed62e363b
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2019-05-14 10:37:58 +0200

    Compute threshold for ddfacet

--- stimela/cargo/cab/ddfacet/src/run.py
+++ stimela/cargo/cab/ddfacet/src/run.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import json
+import pyfits
 
 sys.path.append("/scratch/stimela")
 import utils
@@ -22,6 +23,8 @@ for param in cab['parameters']:
         continue
     if name == 'Parset' and value is None:
         continue
+    if name == 'Noise-Image' and value is None:
+        continue
 
     if isinstance(value, list):
         arg = "{0}{1} {2}".format(cab['prefix'], name, ",".join(value))
@@ -29,6 +32,29 @@ for param in cab['parameters']:
         arg = '{0}{1} {2}'.format(cab['prefix'], name, value)
 
     args.append(arg)
+
+
+removed = False
+for item1 in args:
+    if 'Noise-Image' in item1:
+        noise_image = item1.split('{0}Noise-Image '.format(cab['prefix']))[-1]
+        args.remove('{0}Noise-Image {1}'.format(cab['prefix'], noise_image))
+        noise_hdu = pyfits.open(noise_image)
+        noise_data = noise_hdu[0].data
+        noise_std = noise_data.std()
+        noise_hdu.close()
+        for item2 in args:
+            if 'Noise-Sigma' in item2:
+                noise_sigma = item2.split('{0}Noise-Sigma '.format(cab['prefix']))[-1]
+                args.remove('{0}Noise-Sigma {1}'.format(cab['prefix'], noise_sigma))
+                removed = True
+                threshold = float(noise_sigma)*noise_std
+                for item3 in args:
+                    if '{0}Deconv-FluxThreshold'.format(cab['prefix']) in item3:
+                        args.remove(item3)
+                args.append('{0}Deconv-FluxThreshold {1}'.format(cab['prefix'], threshold))
+if not removed:
+    args.remove('{0}Noise-Sigma 3'.format(cab['prefix']))
 
 if parset is not None:
     args.insert(0, parset)

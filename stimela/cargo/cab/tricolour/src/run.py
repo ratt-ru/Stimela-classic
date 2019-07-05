a9e26a4a2bb4bce9a633a55690d7b29c0a4cf46d
sphemakh <sphemakh@gmail.com>
2019-07-05 15:20:21 +0200

    parse field names properly

--- stimela/cargo/cab/tricolour/src/run.py
+++ stimela/cargo/cab/tricolour/src/run.py
@@ -25,10 +25,16 @@ for param in cab['parameters']:
     elif name == 'ms':
         msname = value
         continue
+    elif name == "field-names":
+        if isinstance(value, str):
+            fields = "--field-names " + value
+        else:
+            fields = "--field-names " + " --field-names ".join(value)
+        continue
 
     args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
 
 if msname:
-    utils.xrun(cab['binary'], args+[msname])
+    utils.xrun(cab['binary'], args+[fields, msname])
 else:
     raise RuntimeError('MS name has not be specified')

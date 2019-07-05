b3d098de7275e471cd0e000107b1d9560b111849
sphemakh <sphemakh@gmail.com>
2019-07-05 12:20:39 +0200

    add tricolour

---
+++ stimela/cargo/cab/tricolour/src/run.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+sys.path.append('/scratch/stimela')
+import utils
+
+
+CONFIG = os.environ["CONFIG"]
+INPUT = os.environ["INPUT"]
+MSDIR = os.environ["MSDIR"]
+
+cab = utils.readJson(CONFIG)
+args = []
+msname = None
+for param in cab['parameters']:
+    name = param['name']
+    value = param['value']
+
+    if value is None:
+        continue
+    elif value is False:
+        continue
+    elif value is True:
+        value = ''
+    elif name == 'msname':
+        if isinstance(value, str):
+            msname = value
+        else:
+            msname = ' '.join(value)
+        continue
+
+    args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
+
+if msname:
+    utils.xrun(cab['binary'], args+[msname])
+else:
+    raise RuntimeError('MS name has not be specified')

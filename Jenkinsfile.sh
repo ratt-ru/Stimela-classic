397a1574450c0c8caca6286901c63bb9b123d36f
sphemakh <sphemakh@gmail.com>
2019-07-31 21:40:31 +0200

    check singularity version on test machine

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -26,6 +26,7 @@ cp -r $TEST_DATA_DIR/beams $TEST_OUTPUT_DIR/input/beams
 
 which stimela
 stimela --version
+singularity -v
 export SINGULARITY_PULLFOLDER=${WORKSPACE_ROOT}/singularity_images
 mkdir $SINGULARITY_PULLFOLDER
 #stimela pull

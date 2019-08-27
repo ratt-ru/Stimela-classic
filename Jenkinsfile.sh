daca11b03ce29ae63a0990941865fc30e2c33362
sphemakh <sphemakh@gmail.com>
2019-08-27 13:37:20 +0200

    setup podman storage on test machine

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -8,8 +8,16 @@ mkdir $TEST_OUTPUT_DIR
 #Custom home for this run's temporary stuff
 HOME=$WORKSPACE_ROOT
 export HOME
+SINGULARITY_STORAGE=${WORKSPACE}/../../../.singularity
 ln -s $WORKSPACE/../../../.udocker ${WORKSPACE_ROOT}/.udocker
-ln -s $WORKSPACE/../../../.singularity ${WORKSPACE_ROOT}/.singularity
+ln -s ${SINGULARITY_STORAGE} ${WORKSPACE_ROOT}/.singularity
+
+# setup podman image storage. Using .singularity volume
+POD_STORAGE=${WORKSPACE_ROOT}/.local/share/containers/storage
+mkdir -p ${POD_STORAGE} ${SINGULARITY_STORAGE}/podman
+ln -s ${SINGULARITY_STORAGE}/podman ${POD_STORAGE}
+
+
 # Install Stimela into a virtual env
 virtualenv ${WORKSPACE_ROOT}/projects/pyenv
 . ${WORKSPACE_ROOT}/projects/pyenv/bin/activate

848be3de841a9e32652c37c3e7e2b7d94fc7136a
Lexy Andati <andatilexy@gmail.com>
2020-04-03 13:18:27 +0200

    Add ragavi_vis cab Set image tag to 1.2.6

---
+++ stimela/cargo/cab/ragavi_vis/src/run.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+sys.path.append('/scratch/stimela')
+
+utils = __import__('utils')
+
+CONFIG = os.environ["CONFIG"]
+INPUT = os.environ["INPUT"]
+MSDIR = os.environ["MSDIR"]
+
+cab = utils.readJson(CONFIG)
+args = []
+
+for param in cab['parameters']:
+    name = param['name']
+    value = param['value']
+    if value is None:
+        continue
+    elif value is False:
+        continue
+
+    if isinstance(value, list):
+        val = map(str, value)
+        args += ['{0}{1} {2}'.format(cab['prefix'], name, " ".join(val))]
+        continue
+
+    args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
+
+utils.xrun(cab['binary'], args)

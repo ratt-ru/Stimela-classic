90809a5512d5f218ee71f475d943a86bb8b2d540
sphemakh <sphemakh@gmail.com>
2020-06-16 13:11:01 +0200

    Use updates base

--- stimela/cargo/cab/casa_polcal/src/run.py
+++ stimela/cargo/cab/casa_polcal/src/run.py
@@ -1,28 +1,35 @@
-import os
-import sys
-import logging
+# -*- coding: future_fstrings -*-
 import Crasa.Crasa as crasa
+from scabha import config, parameters_dict, prun
+from pyrap.tables import table
+import os
+import numpy
 
-sys.path.append("/scratch/stimela")
+print(f"Running CASA task '{config.binary}'")
 
-utils = __import__('utils')
+save_result = parameters_dict.pop("save_result", None)
 
-CONFIG = os.environ["CONFIG"]
-INPUT = os.environ["INPUT"]
-OUTPUT = os.environ["OUTPUT"]
-MSDIR = os.environ["MSDIR"]
-
-cab = utils.readJson(CONFIG)
-
-args = {}
-for param in cab['parameters']:
-    name = param['name']
-    value = param['value']
-
-    if value is None:
-        continue
-
-    args[name] = value
-
-task = crasa.CasaTask(cab["binary"], **args)
+task = crasa.CasaTask(config.binary, save_result=save_result, **parameters_dict)
 task.run()
+
+gtab = parameters_dict["caltable"]
+if not os.path.exists(gtab):
+    raise RuntimeError(f"The gaintable was not created. Please refer to CASA {config.binary} logfile for further details")
+
+tab = table(gtab)
+field_ids = numpy.unique(tab.getcol("FIELD_ID"))
+tab.close()
+
+tab = table(gtab+"::FIELD")
+field_names = tab.getcol("NAME")
+tab.close()
+
+field_in = parameters_dict["field"].split(",")
+
+try:
+    ids = list(map(int, field_in))
+except ValueError:
+    ids = list(map(lambda a: field_names.index(a), field_in))
+
+if not set(ids).issubset(field_ids):
+    raise RuntimeError(f"Some field(s) do not have solutions after the calibration. Please refer to CASA {config.binary} logfile for further details")

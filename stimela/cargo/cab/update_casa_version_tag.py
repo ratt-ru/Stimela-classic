5da4c2d6fa93cdac18b5fccb268e7b293fa71b3a
sphemakh <sphemakh@gmail.com>
2020-06-16 13:28:56 +0200

    back casa-pipeline release

--- stimela/cargo/cab/update_casa_version_tag.py
+++ stimela/cargo/cab/update_casa_version_tag.py
@@ -6,7 +6,7 @@ for cabfile in glob.glob("casa_*/*.json"):
     with open(cabfile, 'rb') as stdr:
         cabdict = json.load(stdr, object_pairs_hook=OrderedDict)
     cabdict["version"] = ["4.7.2", "5.6.1-8"]
-    cabdict["tag"] = ["0.3.0-2", "1.7.0-2"]
+    cabdict["tag"] = ["0.3.0-2", "1.6.3"]
     cabdict["junk"] = ["%s.last" % (cabdict["binary"])]
     with open(cabfile, 'wb') as stdw:
         json.dump(cabdict, stdw, indent=2)

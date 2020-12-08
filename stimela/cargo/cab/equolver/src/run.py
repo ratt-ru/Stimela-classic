fd0b7159b44d1f54a0a82625fe50272de816ce60
gigjozsa <gigjozsast@gmail.com>
2020-12-08 19:55:23 +0200

    Adding equolver

---
+++ stimela/cargo/cab/equolver/src/run.py
@@ -0,0 +1,16 @@
+#config -*- coding: future_fstrings -*-
+import sys
+
+from scabha import config, parse_parameters, prun
+
+args = [config.binary] + parse_parameters(repeat=" ")
+for i in range(len(args)):
+    if args[i] == '--verb':
+        val = args.pop(i+1)
+        if val == 'False':
+            args.pop(i)
+        
+# run the command
+if prun(args) is not 0:
+    sys.exit(1)
+

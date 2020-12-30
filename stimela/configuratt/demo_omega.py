fa6b247f7eae9e7f86802835a8b4ae651c92db42
Oleg Smirnov <osmirnov@gmail.com>
2020-12-30 21:01:40 +0200

    added magic _merge key

--- stimela/configuratt/demo_omega.py
+++ stimela/configuratt/demo_omega.py
@@ -3,6 +3,7 @@ import re
 import os.path
 import munch
 import sys
+from collections.abc import Sequence
 
 from omegaconf.errors import ValidationError
 
@@ -10,6 +11,8 @@ import stimela
 
 
 from omegaconf.omegaconf import MISSING, OmegaConf
+from omegaconf.dictconfig import DictConfig
+from omegaconf.listconfig import ListConfig
 from typing import Any, List, Dict, Optional, Union
 from dataclasses import dataclass, field
 
@@ -44,7 +47,7 @@ class StimelaImage:
     lib: Dict[str, Any] = EmptyDictDefault()
 
     # list of management setups that can be inherited by cabs
-    management: Dict[str, CabManagement] = field(default_factory=lambda:dict(common=CabManagement()))
+    management: CabManagement = CabManagement()
 
 
 ## this is essentially a schema for a cab definition file
@@ -67,7 +70,7 @@ class CabDefinition:
 #    management: CabManagement = CabManagement()
 #    inputs: Optional[Dict[str, CabParameter]] = EmptyDictDefault()
 #    outputs: Optional[Dict[str, CabParameter]] = EmptyDictDefault()
-    management: Optional[Dict[str, Any]] = EmptyDictDefault()
+    management: CabManagement = CabManagement()
     inputs: Optional[Dict[str, Any]] = EmptyDictDefault()
     outputs: Optional[Dict[str, Any]] = EmptyDictDefault()
 
@@ -76,6 +79,50 @@ class StimelaConfig:
     base: Dict[str, StimelaImage] = EmptyDictDefault()
     cab: Dict[str, CabDefinition] = MISSING
 
+
+def lookup_nameseq(name_seq, source_dict):
+    source = source_dict
+    names = list(name_seq)
+    while names:
+        source = source.get(names.pop(0), None)
+        if source is None:
+            return None
+    return source        
+
+def lookup_name(name, *sources):
+    name_seq = name.split(".")
+    for source in sources:
+        result = lookup_nameseq(name_seq, source)
+        if result is not None:
+            return result
+    raise NameError(f"unknown key {name}")
+
+
+def resolve_merges(conf, name, *sources):
+    if isinstance(conf, DictConfig):
+        merge_sections = conf.pop("_merge", None)
+        if merge_sections:
+            if type(merge_sections) is str:
+                merge_sections = [merge_sections]
+            elif not isinstance(merge_sections, Sequence):
+                raise TypeError(f"invalid {name}._merge field of type {type(merge_sections)}")
+            if not len(merge_sections):
+                raise TypeError(f"invalid {name}._merge field of length 0")
+            # convert to actual sections
+            merge_sections = [lookup_name(name, *sources) for name in merge_sections]
+            # merge them all
+            base = merge_sections[0].copy()
+            base.merge_with(*merge_sections[1:])
+            base.merge_with(conf)
+            conf = base
+        # recurse into content
+        for key, value in conf.items_ex(resolve=False):
+            conf[key] = resolve_merges(value, f"{name}.{key}", *sources)
+    elif isinstance(conf, ListConfig):
+        # recurse in
+        for i, value in enumerate(conf._iter_ex(resolve=False)):
+            conf[i] = resolve_merges(value, f"{name}[{i}]", *sources)
+    return conf
 
 if __name__ == "__main__":
 
@@ -93,6 +140,7 @@ if __name__ == "__main__":
     # merge all cab/*/*yml files into the config one by one, under cab[taskname]
     for path in glob.glob(f"{stimela_dir}/cargo/cab/*/*.yaml"):
         cabconf = OmegaConf.load(path)
+        cabconf = resolve_merges(cabconf, "", conf, cabconf)
         cabconf1 = OmegaConf.create(dict(cab={cabconf.task: cabconf}))
         conf = OmegaConf.merge(conf, cabconf1)
 
@@ -100,10 +148,14 @@ if __name__ == "__main__":
 
  #   rootconf = OmegaConf.merge(baseconf, cabconf)
 
-    print(conf.cab.casa_applycal.inputs._includes[0])
-    print(conf.cab.casa_applycal.management)
-    print(conf.cab.casa_applycal.management._includes[0])
+    print(conf.cab.casa_applycal.inputs)
 
     print(OmegaConf.to_yaml(conf, resolve=True))
 
 
+# # %%
+# from omegaconf import OmegaConf
+# cfg = OmegaConf.create({"foo": {"bar" : 10}})
+# cfg.foo.merge_with({"x":  20, "bar": 20})
+# print(cfg)
+# # %%

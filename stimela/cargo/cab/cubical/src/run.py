798af44570a55b7074443673ab5638937dff8c5b
sphemakh <sphemakh@wcap.wits.ac.za>
2021-03-24 15:00:15 +0200

    improve cubical-lsm-api

--- stimela/cargo/cab/cubical/src/run.py
+++ stimela/cargo/cab/cubical/src/run.py
@@ -5,6 +5,8 @@ import shlex
 import configparser
 import ast
 from scabha import config, parameters_dict, prun
+from casacore.tables import table
+import re
 
 args = {}
 parset = []
@@ -19,13 +21,47 @@ for name, value in parameters_dict.items():
     elif name == 'parset':
         parset = [value]
         continue
-    elif isinstance(value, list):
+    elif isinstance(value, list) and name not in ["model-list", "model-expr"]:
         value = ",".join(map(str, value))
 
     args[name] = value
 
 # available jones terms
 joneses = "g b dd".split()
+
+dests = dict(msfile=os.environ["MSDIR"], output=os.environ["OUTPUT"], input=os.environ["INPUT"])
+models = {}
+tab = table(args['data-ms'])
+cols = tab.colnames()
+tab.close()
+for i,model in enumerate(args['model-list']):
+    if model.find(":") > 0:
+        name, dest = model.split(":")
+        dest = dests[dest]
+        model = os.path.join(dest, name)
+    elif model not in cols:
+        model = os.path.join(dests['input'], model)
+    models[i] = model
+if not args.get('model-expr', None):
+    args['model-list'] = ":".join(models.values())
+else:
+    model_list = []
+    for expr in args['model-expr']:
+        if expr.isdigit():
+            model_list.append(models[int(expr)])
+            continue
+        match = re.match(r"(\d+)[\+-](\d+)", expr)
+        if not hasattr(match, 'groups'):
+            raise RuntimeError(f"The expression '{expr}' given to the cubical cab 'model-expr' option is not valid")
+        first, last = match.groups()
+        operand_idx = len(first)
+        operand = expr[operand_idx]
+        if operand == "-":
+            operand = "+-"
+        model_list.append( operand.join([ models[int(first)],models[int(last)] ])  )
+    args['model-list'] = ":".join(model_list)
+        
+args.pop('model-expr', None)
 
 try:
     soljones = args.pop("sol-jones")

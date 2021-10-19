d2589f636b20e2b847a3b1017d8510a549a460b7
Sphesihle Makhathini <sphemakh@gmail.com>
2021-10-19 15:52:26 +0200

    use != and not 'is not'

--- stimela/cargo/cab/mProjectCube/src/run.py
+++ stimela/cargo/cab/mProjectCube/src/run.py
@@ -8,5 +8,5 @@ from scabha import config, parse_parameters, prun
 args = [config.binary] + parse_parameters(repeat=True)
 
 # run the command
-if prun(args) is not 0:
-    sys.exit(1)
+if prun(args) != 0:
+    raise SystemError(f" {config.binary} exited with a non-zero code. See logs for details")

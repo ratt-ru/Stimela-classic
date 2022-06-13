3340e82a29724100180ec7814339fdc88b78c878
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2022-06-13 19:18:23 +0200

    Update singularity.py

--- stimela/singularity.py
+++ stimela/singularity.py
@@ -120,8 +120,6 @@ class Container(object):
         self.status = "running"
         self._print("Starting container [{0:s}]. Timeout set to {1:d}. The container ID is printed below.".format(
             self.name, self.time_out))
-        a = list(args) + [volumes, self.image, self.RUNSCRIPT]
-        print(a)
         utils.xrun(f"cd {self.execdir} && singularity run --workdir {self.execdir} --containall",
 		    list(args) + [volumes, self.image, self.RUNSCRIPT],
                     log=self.logger, timeout=self.time_out, output_wrangler=output_wrangler,

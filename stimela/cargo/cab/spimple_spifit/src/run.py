f165eac1b794a6edbb086b5057eea624a8816cae
mulan-94 <andatilexy@gmail.com>
2022-07-07 12:17:47 +0200

    Small fix

--- stimela/cargo/cab/spimple_spifit/src/run.py
+++ stimela/cargo/cab/spimple_spifit/src/run.py
@@ -4,7 +4,7 @@ import sys
 from scabha import config, parse_parameters, prun
 
 pars = parse_parameters(repeat=" ")
-if "psf-pars" in pars:
+if "--psf-pars" in pars:
     pars = " ".join(pars).split()
 
 args = [config.binary] + pars

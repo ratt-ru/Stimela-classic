2f9d4df42881bdf3342b1bd4cc7dda7039c1a1d3
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2022-09-02 00:39:27 +0200

    Add requirement files

--- setup.py
+++ setup.py
@@ -5,22 +5,20 @@ import sys
 from setuptools import setup
 import glob
 
-
-requirements = [
-                "pyyaml",
-                "nose>=1.3.7",
-                "future-fstrings",
-                "scabha<=0.3.2"
-                ]
-
 PACKAGE_NAME = "stimela"
 __version__ = "1.7.5"
 build_root = os.path.dirname(__file__)
+
 
 def readme():
     """Get readme content for package long description"""
     with open(os.path.join(build_root, 'README.rst')) as f:
         return f.read()
+
+def requirements():
+    """Get package requirements"""
+    with open(os.path.join(build_root, 'requirements.txt')) as f:
+        return [pname.strip() for pname in f.readlines()]
 
 setup(name=PACKAGE_NAME,
       version=__version__,
@@ -42,7 +40,8 @@ setup(name=PACKAGE_NAME,
           "cab/*/src/tdlconf.profiles",
       ]},
       python_requires='>=3.6',
-      install_requires=requirements,
+      install_requires=requirements(),
+      tests_require=['nose'],
       scripts=["bin/" + i for i in os.listdir("bin")] + 
                 glob.glob("stimela/cargo/cab/stimela_runscript"),
       classifiers=[],

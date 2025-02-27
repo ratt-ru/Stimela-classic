f3c6be0f77e78a8e1b04ed87f829a18243ad7f70
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2025-02-27 19:29:35 +0200

    Initial commit of cab file for mosaic queen

---
+++ stimela/cargo/cab/mosaic_queen/src/run.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import subprocess
+import shlex
+import glob
+import shutil
+import yaml
+
+
+CONFIG = os.environ["CONFIG"]
+INPUT = os.environ["INPUT"]
+MSDIR = os.environ["MSDIR"]
+OUTPUT = os.environ["OUTPUT"]
+
+with open(CONFIG, "r") as _std:
+    cab = yaml.safe_load(_std)
+junk = cab["junk"]
+
+args = []
+targets = None
+for param in cab['parameters']:
+    name = param['name']
+    value = param['value']
+
+    if value is None:
+        continue
+    elif value is False:
+        continue
+    elif value is True:
+        value = ''
+    elif dtype == 'list:str' and value is not None:
+        value = " ".join(map(str, value))  # Flatten the list into a string
+        continue
+
+    args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
+
+_runc = " ".join([cab["binary"]] + args)
+
+try:
+    subprocess.check_call(shlex.split(_runc))
+finally:
+    for item in junk:
+        for dest in [OUTPUT, MSDIR]: # these are the only writable volumes in the container
+            items = glob.glob("{dest}/{item}".format(**locals()))
+            for f in items:
+                if os.path.isfile(f):
+                    os.remove(f)
+                elif os.path.isdir(f):
+                    shutil.rmtree(f)

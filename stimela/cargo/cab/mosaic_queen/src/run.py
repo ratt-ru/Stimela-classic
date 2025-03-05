2e09a417870030c2f30964c11eef9d5b96a4fa80
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2025-03-05 20:44:07 +0200

    use scabha

--- stimela/cargo/cab/mosaic_queen/src/run.py
+++ stimela/cargo/cab/mosaic_queen/src/run.py
@@ -1,50 +1,11 @@
-import os
 import sys
-import subprocess
-import shlex
-import glob
-import shutil
-import yaml
 
+from scabha import config, parse_parameters, prun
 
-CONFIG = os.environ["CONFIG"]
-INPUT = os.environ["INPUT"]
-MSDIR = os.environ["MSDIR"]
-OUTPUT = os.environ["OUTPUT"]
+pars = parse_parameters(repeat=" ")
 
-with open(CONFIG, "r") as _std:
-    cab = yaml.safe_load(_std)
-junk = cab["junk"]
-
-args = []
-targets = None
-for param in cab['parameters']:
-    name = param['name']
-    value = param['value']
-    dtype = param['dtype']
-
-    if value is None:
-        continue
-    elif value is False:
-        continue
-    elif value is True:
-        value = ''
-    elif dtype in ['list:file']:
-        value = " ".join(map(str, value))  # Flatten the list into a string
-
-    args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
-
-_runc = " ".join([cab["binary"]] + args)
-print(_runc)
-print(cab['parameters'])
-try:
-    subprocess.check_call(shlex.split(_runc))
-finally:
-    for item in junk:
-        for dest in [OUTPUT, MSDIR]: # these are the only writable volumes in the container
-            items = glob.glob("{dest}/{item}".format(**locals()))
-            for f in items:
-                if os.path.isfile(f):
-                    os.remove(f)
-                elif os.path.isdir(f):
-                    shutil.rmtree(f)
+args = [config.binary] + pars
+    
+# run the command
+if prun(args) != 0:
+    sys.exit(1)

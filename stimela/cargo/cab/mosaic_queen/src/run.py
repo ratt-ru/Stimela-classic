d50f5418ee9cf74972fa604f6380fb96e7f64733
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2025-03-07 13:43:41 +0200

    Updates

--- stimela/cargo/cab/mosaic_queen/src/run.py
+++ stimela/cargo/cab/mosaic_queen/src/run.py
@@ -1,11 +1,15 @@
 import sys
+import os
+
+INDIR = os.environ["INPUT"]
+OUTDIR = os.environ["OUTPUT"]
 
 from scabha import config, parse_parameters, prun
 
 pars = parse_parameters(repeat=" ")
 
-args = [config.binary] + pars
-    
+args = [config.binary, "--input", INDIR + '/', "--output", OUTDIR + '/'] + pars
+ 
 # run the command
 if prun(args) != 0:
     sys.exit(1)

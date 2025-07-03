f4298b08c585a6666391b3ae08723bc8bc0f896c
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2025-07-03 15:06:56 +0200

    Include stimela_runscript

--- stimela/runner.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import os
-import sys
-import subprocess
-
-def main():
-    script_path = os.path.join(os.path.dirname(__file__), 'cargo/cab/stimela_runscript')
-    sys.exit(subprocess.call(["bash", script_path] + sys.argv[1:]))

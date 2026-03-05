ae708a7b4f846499a4505b626e4fc1090ba549a1
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2026-03-06 00:43:32 +0200

    Update params

--- stimela/cargo/cab/mosaic_queen/src/run.py
+++ stimela/cargo/cab/mosaic_queen/src/run.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import subprocess
 import shlex
 import glob
@@ -16,26 +17,21 @@ with open(CONFIG, "r") as _std:
 junk = cab["junk"]
 
 args = []
-for param in cab["parameters"]:
-    name = param["name"]
-    value = param["value"]
-    dtype = param["dtype"]
+for param in cab['parameters']:
+    name = param['name']
+    value = param['value']
+    dtype = param['dtype']
 
     if value is None:
         continue
     elif value is False:
         continue
     elif value is True:
-        value = ""
-    elif dtype in ["list:file", "list:str"]:
-        if os.path.exists(value[0]):
-            indir = os.path.dirname(value[0])
-        value = list(map(os.path.basename, value))
-        value = " ".join(value)
+        value = ''
+    elif dtype in ['list:file', 'list:str']:
+        value = ' '.join(value)
 
-    args += ["{0}{1} {2}".format(cab["prefix"], name, value)]
-
-args += ["--input {0:s} --output {1:s}".format(indir, OUTPUT)]
+    args += ['{0}{1} {2}'.format(cab['prefix'], name, value)]
 
 _runc = " ".join([cab["binary"]] + args)
 
@@ -43,10 +39,7 @@ try:
     subprocess.check_call(shlex.split(_runc))
 finally:
     for item in junk:
-        for dest in [
-            OUTPUT,
-            MSDIR,
-        ]:  # these are the only writable volumes in the container
+        for dest in [OUTPUT, MSDIR]: # these are the only writable volumes in the container
             items = glob.glob("{dest}/{item}".format(**locals()))
             for f in items:
                 if os.path.isfile(f):
